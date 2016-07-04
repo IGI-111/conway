5d7f5951d8229afd4b3b1170c75fde5bcaf0b453
IGI-111 <igi.222@gmail.com>
2016-07-04 10:48:10 +0200

    added command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "conway.hpp"
 #include <SFML/Graphics.hpp>
 #include <thread>
+#include <string>
 
-int main(void) {
-    float width = 400;
-    float height = 400;
-    sf::RenderWindow window(sf::VideoMode(width, height, 1), "Life", sf::Style::Default & ~sf::Style::Resize);
+int main(int argc, char *argv[]) {
+    int width = 100, height = 100;
+    if(argc == 2){
+        width = height = std::stoi(std::string(argv[1]));
+    } else if(argc == 3){
+        width = std::stoi(std::string(argv[1]));
+        height = std::stoi(std::string(argv[2]));
+    }
+
+    sf::RenderWindow window(sf::VideoMode(width, height, 1), "Life", sf::Style::None);
     Conway::Board state(width, height);
 
     while (window.isOpen()) {
